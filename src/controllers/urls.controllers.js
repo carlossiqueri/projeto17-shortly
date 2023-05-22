5e0e826531438a99bc1b39a83709b2d67a9906db
Carlos Siqueri <carlossiqueri@Carlos>
2023-05-22 11:48:35 -0300

    fix bugs at posting urls

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -6,29 +6,20 @@ const urlShorten = async (req, res) => {
   const { authorization } = req.headers;
   const token = authorization?.replace("Bearer ", "");
   const shorterUrl = nanoid(8);
+  const { user } = res.locals;
 
   // Route aut:
   if (!token) return res.sendStatus(401);
 
   try {
-    // first get the user with the token than verify if session is active
-    const verifySession = await db.query(
-      `SELECT * FROM session WHERE token=$1`,
-      [token]
-    );
-    if (verifySession.rows.length === 0) return res.sendStatus(401);
-
-    // saves the verified user in a variable
-    const user = verifySession.rows[0];
-
     await db.query(
-      `INSERT INTO urls(url, shorten_url, user_id) VALUES($1, $2, $3);`,
+      `INSERT INTO urls(url, shorten_url, user_id) VALUES($1, $2, $3) RETURNING id;`,
       [url, shorterUrl, user.user_id]
     );
 
-    const saveUrl = await db.query(`SELECT * FROM urls WHERE url=$1`, [url]);
+    const { id } = result.rows[0];
 
-    res.status(201).send({ id: saveUrl.id, shortUrl: shorterUrl });
+    res.status(201).send({ id: id, shortUrl: shorterUrl });
   } catch (err) {
     res.status(500).send(err.message);
   }
