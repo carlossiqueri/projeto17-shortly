bd98527d25d4189a21e8d51be51efc306fb5770c
Carlos Siqueri <carlossiqueri@Carlos>
2023-05-22 10:25:27 -0300

    feat: handle sign in

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,7 +1,8 @@
 import { db } from "../database/database.connection.js";
 import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
-export async function signUp(req, res) {
+export const signUp = async (req, res) => {
   const { name, email, password, confirmPassword } = req.body;
 
   try {
@@ -10,7 +11,7 @@ export async function signUp(req, res) {
     ]);
     if (checkUser.rows.length !== 0)
       return res.status(409).send("Invalid email: already in use!");
-    if ((password === confirmPassword)) {
+    if (password === confirmPassword) {
       const newPass = bcrypt.hashSync(password, 10);
       await db.query(
         `INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`,
@@ -22,4 +23,30 @@ export async function signUp(req, res) {
   } catch (err) {
     res.status(500).send(err.message);
   }
-}
+};
+
+export const signIn = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await db.query(`SELECT * FROM users WHERE email=$1`, [email]);
+  if (!user.rows[0]) return sendSatus(401);
+
+  const checkPassword = bcrypt.compareSync(password, user.rows[0].password);
+
+  if (!checkPassword) {
+    res.sendStatus(401);
+    return;
+  } else {
+    const token = uuid();
+    try {
+      await db.query(`INSERT INTO session(user_id, token) VALUES ($1, $2)`, [
+        user.rows[0].id,
+        token,
+      ]);
+
+      res.status(200).send(token);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
+};
--- src/routers/users.routes.js
+++ src/routers/users.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validate.middleware.js";
 import { signUpSchema } from "../schemas/userSignUp.schemas.js";
-import { signUp } from "../controllers/users.controllers.js";
+import {signInSchema} from "../schemas/userSignIn.schema.js";
+import { signUp, signIn } from "../controllers/users.controllers.js";
 
 const usersRoutes = Router();
 usersRoutes.post("/signup", validateSchema(signUpSchema), signUp);
+usersRoutes.post("/signIn",validateSchema(signInSchema), signIn);
 
 export default usersRoutes;
