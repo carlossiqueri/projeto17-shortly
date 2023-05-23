294b022a5fff8bc1a432fad804affcf66c8ac74b
Carlos Siqueri <carlossiqueri@Carlos>
2023-05-23 14:06:08 -0300

    feat: show url visit ladder information

---
+++ src/controllers/ranking.controllers.js
@@ -0,0 +1,15 @@
+import { db } from "../database/database.connection.js";
+
+const getRanking = async (req, res) => {
+  try {
+    const urlsLadder = await db.query(`
+    SELECT users.id, name, COUNT (urls.id) AS "linksCount", SUM(visit_count) AS "visitCount" 
+    FROM users LEFT JOIN urls ON users.id = urls.user_id 
+    GROUP BY users.id, visit_count ORDER BY visit_count DESC LIMIT 10
+        `);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+export default getRanking;
--- src/routers/index.routes.js
+++ src/routers/index.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import usersRoutes from "./users.routes.js";
 import urlRoute from "./url.routes.js";
+import rankingRouter from "./ranking.routes.js";
 
 const router = Router();
 router.use(usersRoutes);
 router.use(urlRoute);
+router.use(rankingRouter);
 
 export default router;
